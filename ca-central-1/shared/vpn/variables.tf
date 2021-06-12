0e2ebdd3562c29ddee6bcad6360725ab2cce3445
Anmol Nagpal <ianmolnagpal@gmail.com>
2021-06-12 15:43:30 -0400

    fixing vpn for shared account

--- ca-central-1/shared/vpn/variables.tf
+++ ca-central-1/shared/vpn/variables.tf
@@ -8,7 +8,27 @@ variable "environment" {
   default     = "shared"
 }
 
-variable "subnet_ids{
-    description = ""
-    default = ""
+variable "subnet_ids" {
+  description = ""
+  default     = ""
+}
+
+variable "allowed_ip_external" {
+  description = ""
+  default     = "0.0.0.0/0"
+}
+
+variable "allowed_ip_internal" {
+  description = ""
+  default     = "10.0.0.0/16"
+}
+
+variable "vpc_id" {
+  description = ""
+  default     = ""
+}
+
+variable "public_subnet_ids" {
+  description = ""
+  default     = [""]
 }
